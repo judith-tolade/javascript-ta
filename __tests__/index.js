713cf3f4531676a2ad9cfb82a71c8b36fbf5e447
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2022-06-17 23:13:23 +0100

    Added the solution

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,46 +1,46 @@
 const { solution } = require("../src");
 
-// describe("Sample Test", () => {
-//   it("Test string uppercase", () => {
-//     const output = "foo".toUpperCase();
-//     const expected = "FOO";
-//     expect(output).toStrictEqual(expected);
-//   });
-// });
+describe("Sample Test", () => {
+   it("Test string uppercase", () => {
+     const output = "foo".toUpperCase();
+    const expected = "FOO";
+    expect(output).toStrictEqual(expected);
+   });
+ });
 
 describe("Test solutions", () => {
   it("Testcase 0", () => {
-    const input = 2;
+    const input = "madam";
     const output = solution(input);
     expect(output).toStrictEqual(true);
   });
 
   it("Testcase 1", () => {
-    const input = 24;
+    const input = "random string";
     const output = solution(input);
     expect(output).toStrictEqual(false);
   });
 
   it("Testcase 2", () => {
-    const input = 37;
+    const input = "satanoscillatemymetallicsonatas";
     const output = solution(input);
     expect(output).toStrictEqual(true);
   });
 
   it("Testcase 3", () => {
-    const input = 73;
+    const input = "in girum imus nocte et consumimur igni";
     const output = solution(input);
     expect(output).toStrictEqual(true);
   });
 
   it("Testcase 4", () => {
-    const input = 56;
+    const input = "lmaoml";
     const output = solution(input);
     expect(output).toStrictEqual(false);
   });
 
   it("Testcase 5", () => {
-    const input = 97;
+    const input = "wassamassaw";
     const output = solution(input);
     expect(output).toStrictEqual(true);
   });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // complete the function
-function prime (num) {
+function palindrome(str) {
   // code goes here
+  let reg = /[\W_]/g; //Using regular expression to remove unwanted characters from it
+  let lowStr = str.toLowerCase().replace(reg, ""); //Convert the string to lowercase and a new string retured(replaced) without spaces and commas(as specified in the regExp).
+  let reverseStr = lowStr.split("").reverse().join(""); //Seperate(split) the string into sub-strings, reverse the split string and join them back together
+  return reverseStr === lowStr; //Checking if the reversed string is same as the other previous string converted to lowercase. If it matches then its a palindrome but if it doesn't match then its not a palindrome.
 }
 
 function solution (arg) {
-  return prime(arg);
+  return palindrome(arg);
 }
 
 if (typeof require !== "undefined" && require.main === module) {
@@ -17,3 +21,5 @@ if (typeof require !== "undefined" && require.main === module) {
 }
 
 module.exports = { solution };
+
+
